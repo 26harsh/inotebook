d2a174bc12b7a4b90f168e6f80c2825d41274b34
26harsh <harsh.gc@somaiya.edu>
2024-06-13 14:51:39 +0530

    Routes & Models Created (auth & notes)

--- backend/index.js
+++ backend/index.js
@@ -6,9 +6,23 @@ connectToMongo();
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// Available routes
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/notes', require('./routes/notes'))
+
+// app.get('/', (req, res) => {
+//   res.send('Hello Harsh!')
+// })
+
+// In this way we can create a routes ....but it we will maintain it seperate
+// app.get('/api/v1/login', (req, res) => {
+//   res.send('Hello Login!')
+// })
+
+// app.get('/api/v1/signup', (req, res) => {
+//   res.send('Hello SignUp!')
+// })
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
